3d6709985a65ffbe59febe9fdcea2eb9eb708e9f
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2023-05-03 19:27:49 +0530

    Create Facility 1/3 Part Working

--- Tests/Create_New_Facility.py
+++ Tests/Create_New_Facility.py
@@ -10,65 +10,157 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class CreateNewFacility:
+    # This method is used to create a new facility
     # @pytest.mark.createnewfacility
-
     # def test_Create_Facility(self):
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  # it is calling driver
-    time.sleep(3)  # it is delaying the page for a while
-    # Go to webpage
+
+    # Initialize Chrome driver
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  # This line initializes the driver
+    time.sleep(3)  # This line adds a delay of 3 seconds to the script execution
+
+    # Go to the webpage
     driver.get("https://care.coronasafe.in/")
     time.sleep(5)
-    # Type username nihal to usename field
+
+    # Enter username "Nihal" in the username field
     username_locator = driver.find_element(By.ID, "username")
     username_locator.send_keys("Nihal")
-    # Tyoe password to password field
+
+    # Enter password "Test@123" in the password field
     password_locator = driver.find_element(By.NAME, "password")
     password_locator.send_keys("Test@123")
-    # Push login button
+
+    # Click the login button
     submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
     submit_button.click()
     time.sleep(5)
-    # verify new page url contains & assert use for comparison
+
+    # Verify that the URL of the new page contains "https://care.coronasafe.in/facility"
     actual_url = driver.current_url
     assert actual_url == "https://care.coronasafe.in/facility"
     time.sleep(5)
-    # Navigate to Facility Page
+
+    # Navigate to the Facility Page
     driver.get("https://care.coronasafe.in/facility/create")
     time.sleep(5)
+
     # Wait for the dropdown - Facility Type
     facility_dropdown_menu = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r1:"))
     )
     facility_dropdown_menu.click()
+
     # Wait for the options to appear - Facility Type
     options = WebDriverWait(driver, 20).until(
         EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
     )
+
     # Click the fourth option - Facility Type
     option_four = options[3]
     option_four.click()
     time.sleep(4)
-    #
-    # Facility Name
+
+    # Fill in the Facility Name
     Facility_Name = driver.find_element(By.NAME, "name")
     Facility_Name.send_keys("Selenium")
     time.sleep(4)
-    #Features
-    #Pincode
+
+    # Select Features
+
+    # Wait for the dropdown - Feature Type
+    # feature_dropdown_menu = WebDriverWait(driver, 20).until(
+    #     EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r3:"))
+    # )
+    # feature_dropdown_menu.click()
+    #
+    # # Wait for the options to appear - Feature Type
+    # options = WebDriverWait(driver, 20).until(
+    #     EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+    # )
+    #
+    # # Click the third and fourth options - Feature Type
+    # option_three = options[2]
+    # option_three.click()
+    # option_four = options[3]
+    # option_four.click()
+    # time.sleep(4)
+
+    # Fill in the Pincode
     Facility_pincode = driver.find_element(By.NAME, "pincode")
     Facility_pincode.send_keys("682011")
     time.sleep(4)
-    #State
-    #District
-    #Local Body
-    #Ward
-    #Address
-    Facility_address = driver.find_element(By.NAME, "address")
-    Facility_address.send_keys("kasaragod,kerala,india")
+
+    # State
+    state_dropdown_menu = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r5:"))
+    )
+    state_dropdown_menu.click()
+    # Wait for the options to appear - State type
+    options = WebDriverWait(driver, 20).until(
+        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+    )
+    # Click the first option - State type
+    option_one = options[0]
+    option_one.click()
     time.sleep(4)
-    #Emergency
-    Facility_emergency_number = driver.find_element(By.NAME, "phone_number")
-    Facility_emergency_number.send_keys("9999999999")
+
+    # District
+    district_dropdown_menu = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r7:"))
+    )
+    district_dropdown_menu.click()
+    # Wait for the options to appear - District type
+    options = WebDriverWait(driver, 20).until(
+        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+    )
+    # Click the second option - District type
+    option_two = options[1]
+    option_two.click()
     time.sleep(4)
-    #Cylindar
-    #Location
+
+    # Local Body
+    localbody_dropdown_menu = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r9:"))
+    )
+    localbody_dropdown_menu.click()
+    # Wait for the options to appear - localbody type
+    options = WebDriverWait(driver, 20).until(
+        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+    )
+    # Click the second option - localbody type
+    option_two = options[1]
+    option_two.click()
+    time.sleep(4)
+
+    # Ward
+    ward_dropdown_menu = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:rb:"))
+    )
+    ward_dropdown_menu.click()
+    # Wait for the options to appear - ward type
+    options = WebDriverWait(driver, 20).until(
+        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+    )
+    # Click the second option - localbody type
+    option_two = options[1]
+    option_two.click()
+    time.sleep(4)
+
+    # Address
+    facility_address = driver.find_element(By.NAME, "address")
+    facility_address.send_keys("Kasaragod, Kerala, India")
+    time.sleep(4)
+
+    # Emergency
+    facility_emergency_number = driver.find_element(By.NAME, "phone_number")
+    facility_emergency_number.send_keys("9999999999")
+    time.sleep(4)
+
+    # Cylinder
+
+    # Location
+
+    #enter submit button
+    facility_submit_button = driver.find_element(By.ID, "submit")
+    facility_submit_button()
+    time.sleep(5)
