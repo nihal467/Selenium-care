e53a6d1a2bd89ec3a594719863b9ef324075db0f
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2023-05-07 00:35:17 +0530

    Create New Patient

--- Tests/Create_New_Facility.py
+++ Tests/Create_New_Facility.py
@@ -9,220 +9,220 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class CreateNewFacility:
+class TestCreateNewFacility:
     # This method is used to create a new facility
-    # @pytest.mark.createnewfacility
-    # def test_Create_Facility(self):
+    @pytest.mark.createnewfacility #Pytest name and class name can't be same
+    def test_Create_Facility(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  # This line initializes the driver
+        time.sleep(3)  # This line adds a delay of 3 seconds to the script execution
 
-    # Initialize Chrome driver
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  # This line initializes the driver
-    time.sleep(3)  # This line adds a delay of 3 seconds to the script execution
+        # Go to the webpage
+        driver.get("https://care.coronasafe.in/")
+        time.sleep(5)
 
-    # Go to the webpage
-    driver.get("https://care.coronasafe.in/")
-    time.sleep(5)
+        # Enter username "Nihal" in the username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("Nihal")
 
-    # Enter username "Nihal" in the username field
-    username_locator = driver.find_element(By.ID, "username")
-    username_locator.send_keys("Nihal")
+        # Enter password "Test@123" in the password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Test@123")
 
-    # Enter password "Test@123" in the password field
-    password_locator = driver.find_element(By.NAME, "password")
-    password_locator.send_keys("Test@123")
+        # Click the login button
+        submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+        submit_button.click()
+        time.sleep(5)
 
-    # Click the login button
-    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-    submit_button.click()
-    time.sleep(5)
+        # Verify that the URL of the new page contains "https://care.coronasafe.in/facility"
+        actual_url = driver.current_url
+        assert actual_url == "https://care.coronasafe.in/facility"
+        time.sleep(5)
 
-    # Verify that the URL of the new page contains "https://care.coronasafe.in/facility"
-    actual_url = driver.current_url
-    assert actual_url == "https://care.coronasafe.in/facility"
-    time.sleep(5)
+        # Navigate to the Facility Page
+        driver.get("https://care.coronasafe.in/facility/create")
+        time.sleep(5)
 
-    # Navigate to the Facility Page
-    driver.get("https://care.coronasafe.in/facility/create")
-    time.sleep(5)
+        # Wait for the dropdown - Facility Type
+        facility_dropdown_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r1:"))
+        )
+        facility_dropdown_menu.click()
 
-    # Wait for the dropdown - Facility Type
-    facility_dropdown_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r1:"))
-    )
-    facility_dropdown_menu.click()
+        # Wait for the options to appear - Facility Type
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
 
-    # Wait for the options to appear - Facility Type
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
+        # Click the fourth option - Facility Type
+        option_four = options[3]
+        option_four.click()
+        time.sleep(4)
 
-    # Click the fourth option - Facility Type
-    option_four = options[3]
-    option_four.click()
-    time.sleep(4)
+        # Fill in the Facility Name
+        Facility_Name = driver.find_element(By.NAME, "name")
+        Facility_Name.send_keys("Selenium")
+        time.sleep(4)
 
-    # Fill in the Facility Name
-    Facility_Name = driver.find_element(By.NAME, "name")
-    Facility_Name.send_keys("Selenium")
-    time.sleep(4)
+        # Select Features
 
-    # Select Features
+        # Wait for the dropdown - Feature Type
+        # feature_dropdown_menu = WebDriverWait(driver, 20).until(
+        #     EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r3:"))
+        # )
+        # feature_dropdown_menu.click()
+        #
+        # # Wait for the options to appear - Feature Type
+        # options = WebDriverWait(driver, 20).until(
+        #     EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        # )
+        #
+        # # Click the third and fourth options - Feature Type
+        # option_three = options[2]
+        # option_three.click()
+        # option_four = options[3]
+        # option_four.click()
+        # time.sleep(4)
 
-    # Wait for the dropdown - Feature Type
-    # feature_dropdown_menu = WebDriverWait(driver, 20).until(
-    #     EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r3:"))
-    # )
-    # feature_dropdown_menu.click()
-    #
-    # # Wait for the options to appear - Feature Type
-    # options = WebDriverWait(driver, 20).until(
-    #     EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    # )
-    #
-    # # Click the third and fourth options - Feature Type
-    # option_three = options[2]
-    # option_three.click()
-    # option_four = options[3]
-    # option_four.click()
-    # time.sleep(4)
+        # Fill in the Pincode
+        Facility_pincode = driver.find_element(By.NAME, "pincode")
+        Facility_pincode.send_keys("682011")
+        time.sleep(4)
 
-    # Fill in the Pincode
-    Facility_pincode = driver.find_element(By.NAME, "pincode")
-    Facility_pincode.send_keys("682011")
-    time.sleep(4)
+        # State
+        state_dropdown_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r5:"))
+        )
+        state_dropdown_menu.click()
+        # Wait for the options to appear - State type
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
+        # Click the first option - State type
+        option_one = options[0]
+        option_one.click()
+        time.sleep(4)
 
-    # State
-    state_dropdown_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r5:"))
-    )
-    state_dropdown_menu.click()
-    # Wait for the options to appear - State type
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
-    # Click the first option - State type
-    option_one = options[0]
-    option_one.click()
-    time.sleep(4)
+        # District
+        district_dropdown_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r7:"))
+        )
+        district_dropdown_menu.click()
+        # Wait for the options to appear - District type
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
+        # Click the second option - District type
+        option_two = options[1]
+        option_two.click()
+        time.sleep(4)
 
-    # District
-    district_dropdown_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r7:"))
-    )
-    district_dropdown_menu.click()
-    # Wait for the options to appear - District type
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
-    # Click the second option - District type
-    option_two = options[1]
-    option_two.click()
-    time.sleep(4)
+        # Local Body
+        localbody_dropdown_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r9:"))
+        )
+        localbody_dropdown_menu.click()
+        # Wait for the options to appear - localbody type
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
+        # Click the second option - localbody type
+        option_two = options[1]
+        option_two.click()
+        time.sleep(4)
 
-    # Local Body
-    localbody_dropdown_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:r9:"))
-    )
-    localbody_dropdown_menu.click()
-    # Wait for the options to appear - localbody type
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
-    # Click the second option - localbody type
-    option_two = options[1]
-    option_two.click()
-    time.sleep(4)
+        # Ward
+        ward_dropdown_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:rb:"))
+        )
+        ward_dropdown_menu.click()
+        # Wait for the options to appear - ward type
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
+        # Click the second option - localbody type
+        option_two = options[1]
+        option_two.click()
+        time.sleep(4)
 
-    # Ward
-    ward_dropdown_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "headlessui-listbox-button-:rb:"))
-    )
-    ward_dropdown_menu.click()
-    # Wait for the options to appear - ward type
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
-    # Click the second option - localbody type
-    option_two = options[1]
-    option_two.click()
-    time.sleep(4)
+        # Address
+        facility_address = driver.find_element(By.NAME, "address")
+        facility_address.send_keys("Kasaragod, Kerala, India")
+        time.sleep(4)
 
-    # Address
-    facility_address = driver.find_element(By.NAME, "address")
-    facility_address.send_keys("Kasaragod, Kerala, India")
-    time.sleep(4)
+        # Emergency
+        facility_emergency_number = driver.find_element(By.NAME, "phone_number")
+        facility_emergency_number.send_keys("9999999999")
+        time.sleep(4)
 
-    # Emergency
-    facility_emergency_number = driver.find_element(By.NAME, "phone_number")
-    facility_emergency_number.send_keys("9999999999")
-    time.sleep(4)
+        # Cylinder
 
-    # Cylinder
+        # Location
 
-    # Location
+        #enter submit button
+        facility_submit_button = driver.find_element(By.ID, "submit")
+        facility_submit_button.click()
+        time.sleep(5)
 
-    #enter submit button
-    facility_submit_button = driver.find_element(By.ID, "submit")
-    facility_submit_button.click()
-    time.sleep(5)
+        #-----------------------------------------Section 2 ---------------------------------------------------
 
-    #-----------------------------------------Section 2 ---------------------------------------------------
+        # Bed Type
+        bedtype_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID,"bed-type"))
+        )
+        bedtype_menu.click()
+        # Wait for the options to appear - bedtype
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
+        # Click the second option - bedtype type
+        option_two = options[3]
+        option_two.click()
+        time.sleep(4)
 
-    # Bed Type
-    bedtype_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID,"bed-type"))
-    )
-    bedtype_menu.click()
-    # Wait for the options to appear - bedtype
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
-    # Click the second option - bedtype type
-    option_two = options[3]
-    option_two.click()
-    time.sleep(4)
+        # Total Capacity
+        bed_total_capacity = driver.find_element(By.NAME, "totalCapacity")
+        bed_total_capacity.send_keys("121")
+        time.sleep(4)
 
-    # Total Capacity
-    bed_total_capacity = driver.find_element(By.NAME, "totalCapacity")
-    bed_total_capacity.send_keys("121")
-    time.sleep(4)
+        # Currently Occupied
 
-    # Currently Occupied
+        bed_currently_occupied = driver.find_element(By.NAME, "currentOccupancy")
+        bed_currently_occupied.send_keys("100")
+        time.sleep(4)
 
-    bed_currently_occupied = driver.find_element(By.NAME, "currentOccupancy")
-    bed_currently_occupied.send_keys("100")
-    time.sleep(4)
+        # Submit Save Bed Capacity
 
-    # Submit Save Bed Capacity
+        save_bed_capacity = driver.find_element(By.ID, "bed-capacity-save-and-exit")
+        save_bed_capacity.click()
+        time.sleep(5)
 
-    save_bed_capacity = driver.find_element(By.ID, "bed-capacity-save-and-exit")
-    save_bed_capacity.click()
-    time.sleep(5)
+     #-----------------------------------------Section 3 ---------------------------------------------------
 
- #-----------------------------------------Section 3 ---------------------------------------------------
+        #Area Of Specialisation
+        area_menu = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "area-of-specialization"))
+        )
+        area_menu.click()
+        # Wait for the options to appear - bedtype
+        options = WebDriverWait(driver, 20).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
+        )
 
-    #Area Of Specialisation
-    area_menu = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "area-of-specialization"))
-    )
-    area_menu.click()
-    # Wait for the options to appear - bedtype
-    options = WebDriverWait(driver, 20).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//ul[@role='listbox']/li[@role='option']"))
-    )
+        # Click the second option - bedtype type
+        option_two = options[3]
+        option_two.click()
+        time.sleep(4)
 
-    # Click the second option - bedtype type
-    option_two = options[3]
-    option_two.click()
-    time.sleep(4)
+        #Count
 
-    #Count
+        area_count = driver.find_element(By.NAME, "count")
+        area_count.send_keys("100")
+        time.sleep(4)
 
-    area_count = driver.find_element(By.NAME, "count")
-    area_count.send_keys("100")
-    time.sleep(4)
+        # Submit Specialisation Count
 
-    # Submit Specialisation Count
+        save_area = driver.find_element(By.ID, "save-and-exit")
+        save_area.click()
+        time.sleep(5)
 
-    save_area = driver.find_element(By.ID, "save-and-exit")
-    save_area.click()
-    time.sleep(5)
+        driver.quit() #Always this in the end for the driver to work properly
--- Tests/Create_New_Patient.py
+++ Tests/Create_New_Patient.py
@@ -1,72 +1,75 @@
 import time
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class CreateNewPatient:
-    # This method is used to create a new facility
-    # @pytest.mark.createpatient
-    #def test_Create_Patient(self):
+class TestCreateNewPatient:
+    # This method is used to create a new patient
+    @pytest.mark.createpatient
+    def Create_Patient(self):
 
-    # Initialize Chrome driver
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))  # This line initializes the driver
-    time.sleep(3)  # This line adds a delay of 3 seconds to the script execution
+        # Initialize Chrome driver
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()))  # This line initializes the driver
+        time.sleep(3)  # This line adds a delay of 3 seconds to the script execution
 
-    # Go to the webpage
-    driver.get("https://care.coronasafe.in/")
-    time.sleep(5)
+        # Go to the webpage
+        driver.get("https://care.coronasafe.in/")
+        time.sleep(5)
 
-    # Enter username "Nihal" in the username field
-    username_locator = driver.find_element(By.ID, "username")
-    username_locator.send_keys("Nihal")
+        # Enter username "Nihal" in the username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("Nihal")
 
-    # Enter password "Test@123" in the password field
-    password_locator = driver.find_element(By.NAME, "password")
-    password_locator.send_keys("Test@123")
+        # Enter password "Test@123" in the password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Test@123")
 
-    # Click the login button
-    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-    submit_button.click()
-    time.sleep(5)
+        # Click the login button
+        submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+        submit_button.click()
+        time.sleep(5)
 
-    # Verify that the URL of the new page contains "https://care.coronasafe.in/facility"
-    actual_url = driver.current_url
-    assert actual_url == "https://care.coronasafe.in/facility"
-    time.sleep(5)
+        # Verify that the URL of the new page contains "https://care.coronasafe.in/facility"
+        actual_url = driver.current_url
+        assert actual_url == "https://care.coronasafe.in/facility"
+        time.sleep(5)
 
-    # Search for Facility
+        # Search for Facility
 
-    facility_search= driver.find_element(By.NAME, "search")
-    facility_search.click()
-    facility_search.send_keys("Selenium")
-    time.sleep(3)
+        facility_search= driver.find_element(By.NAME, "search")
+        facility_search.click()
+        facility_search.send_keys("Selenium")
+        time.sleep(3)
 
-    # Go Inside a facility page start with selenium
+        # Go Inside a facility page start with selenium
 
-    facility_view= driver.find_element(By.ID,"facility-details")
-    facility_view.click()
-    time.sleep(3)
+        facility_view= driver.find_element(By.ID,"facility-details")
+        facility_view.click()
+        time.sleep(3)
 
-    # Click Manage Patient
+        # Click Manage Patient
 
-    patient_view=driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/main/div/div/div[3]/div/div[2]/div[2]/button[3]")
-    patient_view.click()
-    time.sleep(3)
+        patient_view=driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/main/div/div/div[3]/div/div[2]/div[2]/button[3]")
+        patient_view.click()
+        time.sleep(3)
 
-    # Click Add patient button in the patient tab
+        # Click Add patient button in the patient tab
 
-    add_patient_details=driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/main/div/div/div[2]/div[2]/button[2]")
-    add_patient_details.click()
-    time.sleep(3)
+        add_patient_details=driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/main/div/div/div[2]/div[2]/button[2]")
+        add_patient_details.click()
+        time.sleep(3)
+        driver.quit()
 
-    # Click the pop-up and select the selenium facility
+        # Click the pop-up and select the selenium facility
 
-    # Fill the Patient Form
-    # Save the patient details
-    # Create a new consultation for the patient
-    # Create a go to the patient consultation page
-    # Verify the redirection URL
+        # Fill the Patient Form
+        # Save the patient details
+        # Create a new consultation for the patient
+        # Create a go to the patient consultation page
+        # Verify the redirection URL
+
